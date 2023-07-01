bdd0d31e929781aabf87048e5ee9964e61666884
Christy Fury <christy.fury@gmail.com>
2023-07-01 16:21:07 -0400

    render documentation updates to static dirs

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 # install dependencies
 pip install -r deps.txt
 
+#collect static files
+python manage.py collectstatic --no-input
+
 # run migration
 python manage.py migrate
 
-# collect static files
-python manage.py collectstatic
--- plants_collector/settings.py
+++ plants_collector/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -151,7 +152,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
 
 # Default primary key field type
